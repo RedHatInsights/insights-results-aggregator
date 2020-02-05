8335ddd2ebe0b7943271f0ed390a74689b8e1933
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-05 09:15:03 +0100

    Stub for real Kafka consumer

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -17,24 +17,65 @@ limitations under the License.
 package consumer
 
 import (
+	"github.com/Shopify/sarama"
+	"log"
+
 	"github.com/RedHatInsights/insights-results-aggregator/broker"
 )
 
 // Consumer represents any consumer of insights-rules messages
 type Consumer interface {
 	Start() error
+	Close() error
 }
 
 // Impl in an implementation of Consumer interface
 type Impl struct {
+	Configuration     broker.Configuration
+	Consumer          sarama.Consumer
+	PartitionConsumer sarama.PartitionConsumer
 }
 
 // New constructs new implementation of Consumer interface
-func New(brokerCfg broker.Configuration) Consumer {
-	return Impl{}
+func New(brokerCfg broker.Configuration) (Consumer, error) {
+	c, err := sarama.NewConsumer([]string{brokerCfg.Address}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	partitions, err := c.Partitions(brokerCfg.Topic)
+
+	partitionConsumer, err := c.ConsumePartition(brokerCfg.Topic, partitions[0], sarama.OffsetNewest)
+	if err != nil {
+		return nil, err
+	}
+
+	consumer := Impl{
+		Configuration:     brokerCfg,
+		Consumer:          c,
+		PartitionConsumer: partitionConsumer,
+	}
+	return consumer, nil
 }
 
 // Start starts consumer
 func (consumer Impl) Start() error {
+	log.Printf("Consumer has been started, waiting for messages send to topic %s\n", consumer.Configuration.Topic)
+	consumed := 0
+	for {
+		msg := <-consumer.PartitionConsumer.Messages()
+		log.Printf("Consumed message offset %d\n", msg.Offset)
+		consumed++
+	}
 	return nil
+}
+
+// Close method closes all resources used by consumer
+func (consumer Impl) Close() error {
+	err := consumer.PartitionConsumer.Close()
+	if err != nil {
+		return err
+	}
+	err = consumer.Consumer.Close()
+	return err
 }
