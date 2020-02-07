df1953010fc3147369123b54815f5bff52bbfbea
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-07 14:26:07 +0100

    Data types as a package

---
+++ types/types.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2020 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package types
+
+// OrgID represents organization ID
+type OrgID int
+
+// ClusterName represents name of cluster in format c8590f31-e97e-4b85-b506-c45ce1911a12
+type ClusterName string
+
+// ClusterReport represents cluster report
+type ClusterReport string
+
+// Timestamp represents any timestamp in a form gathered from database
+// TODO: need to be improved
+type Timestamp string
