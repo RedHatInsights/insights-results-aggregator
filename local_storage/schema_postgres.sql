209758c41bdc3439e48e1896f163664b00490e0c
bond95 <cyberbond95@gmail.com>
2020-02-13 16:09:32 +0100

    Added postgres support

---
+++ local_storage/schema_postgres.sql
@@ -0,0 +1,10 @@
+--PRAGMA foreign_keys = ON;
+--CREATE SCHEMA public;
+
+create table report (
+    org_id      integer not null,
+    cluster     varchar not null unique,
+    report      varchar not null,
+    reported_at timestamp,
+    PRIMARY KEY(org_id, cluster)
+);
