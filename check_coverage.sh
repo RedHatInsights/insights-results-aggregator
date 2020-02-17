828fd3ee18eceeb8d94024304e42048557f53751
Serhii Zakharov <szakharo@redhat.com>
2020-02-17 11:15:20 +0100

    code coverage

--- check_coverage.sh
+++ check_coverage.sh
@@ -3,7 +3,11 @@
 THRESHOLD=95
 ERR_MESSAGE="Code coverage have to be at least $THRESHOLD%"
 
-if (( $(go tool cover -func=coverage.out | grep -i total | awk '{print $NF}' | egrep "^[-1-9]+" -o) > $THRESHOLD )); then
+go_tool_cover_output=$(go tool cover -func=coverage.out)
+
+echo $go_tool_cover_output
+
+if (( $(echo $go_tool_cover_output | grep -i total | awk '{print $NF}' | egrep "^[-1-9]+" -o) > $THRESHOLD )); then
     exit 0
 else
     echo -e "\033[31m$ERR_MESSAGE\e[0m"
