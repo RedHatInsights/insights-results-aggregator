cdcd25ae046072f366b4ec37d9c27f32ebe657e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-25 11:54:12 +0100

    Removed useless cat from script create_database_sqlite

--- local_storage/create_database_sqlite.sh
+++ local_storage/create_database_sqlite.sh
@@ -19,7 +19,7 @@ DATABASE=aggregator.db
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "Creating DB schema"
-cat "${SCRIPT_DIR}/schema_sqlite.sql" | sqlite3 "${SCRIPT_DIR}/../${DATABASE}"
+sqlite3 "${SCRIPT_DIR}/../${DATABASE}" < "${SCRIPT_DIR}/schema_sqlite.sql"
 
 echo "Inserting test data"
-cat "${SCRIPT_DIR}/test_data_sqlite.sql" | sqlite3 "${SCRIPT_DIR}/../${DATABASE}"
+sqlite3 "${SCRIPT_DIR}/../${DATABASE}" < "${SCRIPT_DIR}/test_data_sqlite.sql"
