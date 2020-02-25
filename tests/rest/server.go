b6b96b7976bec027c6cc2f8b2870d6a55ef741d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-25 13:48:34 +0100

    Minor REST API tests improvements

--- tests/rest/server.go
+++ tests/rest/server.go
@@ -20,14 +20,16 @@ import "github.com/verdverm/frisby"
 
 const apiURL = "http://localhost:8080/api/v1/"
 
+// checkRestAPIEntryPoint check if the entry point (usually /api/v1/) responds correctly to HTTP GET command
 func checkRestAPIEntryPoint() {
-	f := frisby.Create("Check the entry point to REST API").Get(apiURL)
+	f := frisby.Create("Check the entry point to REST API using HTTP GET method").Get(apiURL)
 	f.Send()
 	f.ExpectStatus(200)
 	f.ExpectHeader("Content-Type", "application/json; charset=utf-8")
 	f.PrintReport()
 }
 
+// checkNonExistentEntryPoint check whether non-existing endpoints are handled properly (HTTP code 404 etc.)
 func checkNonExistentEntryPoint() {
 	f := frisby.Create("Check the non-existent entry point to REST API").Get(apiURL + "foobar")
 	f.Send()
@@ -36,35 +38,52 @@ func checkNonExistentEntryPoint() {
 	f.PrintReport()
 }
 
+// checkWrongEntryPoint check whether wrongly scecified URLs are handled correctly
 func checkWrongEntryPoint() {
-	f := frisby.Create("Check the wrong entry point to REST API").Get(apiURL + "../")
+	postfixes := [...]string{"..", "../", "...", "..?", "..?foobar"}
+	for _, postfix := range postfixes {
+		f := frisby.Create("Check the wrong entry point to REST API with postfix '" + postfix + "'").Get(apiURL + postfix)
+		f.Send()
+		f.ExpectStatus(404)
+		f.ExpectHeader("Content-Type", "text/plain; charset=utf-8")
+		f.PrintReport()
+	}
+}
+
+// sendAndExpectStatus sends the request to the server and checks whether expected HTTP code (status) is returned
+func sendAndExpectStatus(f *frisby.Frisby, expectedStatus int) {
 	f.Send()
-	f.ExpectStatus(404)
-	f.ExpectHeader("Content-Type", "text/plain; charset=utf-8")
+	f.ExpectStatus(405)
 	f.PrintReport()
 }
 
+// check whether other HTTP methods are rejected correctly
 func checkWrongMethodsForEntryPoint() {
 	f := frisby.Create("Check the entry point to REST API with wrong method: POST").Post(apiURL)
-	f.Send()
-	f.ExpectStatus(405)
-	f.PrintReport()
+	sendAndExpectStatus(f, 405)
 
 	f = frisby.Create("Check the entry point to REST API with wrong method: PUT").Put(apiURL)
-	f.Send()
-	f.ExpectStatus(405)
-	f.PrintReport()
+	sendAndExpectStatus(f, 405)
 
 	f = frisby.Create("Check the entry point to REST API with wrong method: DELETE").Delete(apiURL)
-	f.Send()
-	f.ExpectStatus(405)
-	f.PrintReport()
+	sendAndExpectStatus(f, 405)
+
+	f = frisby.Create("Check the entry point to REST API with wrong method: PATCH").Patch(apiURL)
+	sendAndExpectStatus(f, 405)
+
+	f = frisby.Create("Check the entry point to REST API with wrong method: OPTIONS").Options(apiURL)
+	sendAndExpectStatus(f, 405)
+
+	f = frisby.Create("Check the entry point to REST API with wrong method: HEAD").Head(apiURL)
+	sendAndExpectStatus(f, 405)
 }
 
+// checkOpenAPISpecifications checks whether OpenAPI endpoint is handled correctly
 func checkOpenAPISpecifications() {
 	f := frisby.Create("Check the wrong entry point to REST API").Get(apiURL + "openapi.json")
 	f.Send()
 	f.ExpectStatus(200)
+	f.ExpectHeader("Content-Type", "application/json")
 	f.PrintReport()
 }
 
