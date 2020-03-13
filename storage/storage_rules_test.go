13245bbf14744df0a37f33543e032aeb85630cdd
Serhii Zakharov <szakharo@redhat.com>
2020-03-13 16:35:09 +0100

    fixed order of items

--- storage/storage_rules_test.go
+++ storage/storage_rules_test.go
@@ -1,6 +1,21 @@
+// Copyright 2020 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package storage_test
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/RedHatInsights/insights-results-aggregator/content"
@@ -324,6 +339,11 @@ func TestDBStorageGetContentForMultipleRulesOK(t *testing.T) {
 	helpers.FailOnError(t, err)
 
 	assert.Len(t, res, 3)
+
+	// db doesn't and shouldn't guarantee order
+	sort.Slice(res, func(firstIndex, secondIndex int) bool {
+		return res[firstIndex].ErrorKey < res[secondIndex].ErrorKey
+	})
 
 	// TODO: check risk of change when it will be returned correctly
 	// total risk is `(impact + likelihood) / 2`
