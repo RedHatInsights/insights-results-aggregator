167e29a2c4d84ef869a55249599895b5e1fb1ff6
Serhii Zakharov <szakharo@redhat.com>
2020-03-23 20:07:47 +0100

    little refactoring

--- migration/migration.go
+++ migration/migration.go
@@ -25,7 +25,7 @@ package migration
 import (
 	"database/sql"
 	"fmt"
-	"strings"
+	"log"
 )
 
 // Version represents a version of the database.
@@ -60,24 +60,32 @@ func GetMaxVersion() Version {
 // If it already exists, no changes will be made to the database.
 // Otherwise, a new migration information table will be created and initialized.
 func InitInfoTable(db *sql.DB) error {
-	// Check if migration info table already exists.
-	countResult := db.QueryRow("SELECT COUNT(*) FROM migration_info")
-	var rowCount int
-	err := countResult.Scan(&rowCount)
-	// If it exists, it must have exactly 1 row.
-	// If it doesn't exist, the "no such table" error is expected.
-	// Otherwise, there's something wrong.
-	if err == nil {
+	return withTransaction(db, func(tx *sql.Tx) error {
+		_, err := tx.Exec("CREATE TABLE IF NOT EXISTS migration_info (version INTEGER NOT NULL);")
+		if err != nil {
+			return err
+		}
+
+		// INSERT if there's no rows in the table
+		_, err = tx.Exec(`
+			INSERT INTO migration_info (version) SELECT 0 WHERE NOT EXISTS (SELECT version FROM migration_info);
+		`)
+		if err != nil {
+			return err
+		}
+
+		var rowCount uint
+		err = tx.QueryRow("SELECT COUNT(*) FROM migration_info").Scan(&rowCount)
+		if err != nil {
+			return err
+		}
+
 		if rowCount != 1 {
 			return fmt.Errorf("unexpected number of rows in migration info table (expected: 1, reality: %d)", rowCount)
 		}
-		return nil
-	} else if !strings.Contains(err.Error(), "migration_info") {
-		// An error not related to the nonexistence of the migration_info table.
-		return err
-	}
 
-	return initInfoTab(db)
+		return nil
+	})
 }
 
 // GetDBVersion reads the current version of the database from the migration info table.
@@ -87,7 +95,8 @@ func GetDBVersion(db *sql.DB) (Version, error) {
 		return 0, err
 	}
 	defer func() {
-		_ = rows.Close()
+		err = rows.Close()
+		log.Println("GetDBVersion(): Error during closing rows")
 	}()
 
 	// Read the first (and hopefully the only) row in the table.
@@ -131,28 +140,6 @@ func SetDBVersion(db *sql.DB, targetVer Version) error {
 	return execStepsInTx(db, currentVer, targetVer)
 }
 
-// initInfoTab performs the actual creation and initialization of the migration info table.
-func initInfoTab(db *sql.DB) error {
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
-
-	_, err = tx.Exec("CREATE TABLE migration_info (version INTEGER NOT NULL)")
-	if err != nil {
-		_ = tx.Rollback()
-		return err
-	}
-
-	_, err = tx.Exec("INSERT INTO migration_info(version) VALUES(0)")
-	if err != nil {
-		_ = tx.Rollback()
-		return err
-	}
-
-	return tx.Commit()
-}
-
 // updateVersionInDB updates the migration version number in the migration info table.
 // This function does NOT rollback in case of an error. The calling function is expected to do that.
 func updateVersionInDB(tx *sql.Tx, newVersion Version) error {
@@ -181,34 +168,27 @@ func execStepsInTx(db *sql.DB, currentVer, targetVer Version) error {
 		return nil
 	}
 
-	// Begin a new transaction.
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
+	return withTransaction(db, func(tx *sql.Tx) error {
+		// Upgrade to target version.
+		for currentVer < targetVer {
+			if err := migrations[currentVer].StepUp(tx); err != nil {
+				return err
+			}
+			currentVer++
+		}
 
-	// Upgrade to target version.
-	for currentVer < targetVer {
-		if err = migrations[currentVer].StepUp(tx); err != nil {
-			_ = tx.Rollback()
+		// Downgrade to target version.
+		for currentVer > targetVer {
+			if err := migrations[currentVer-1].StepDown(tx); err != nil {
+				return err
+			}
+			currentVer--
+		}
+
+		if err := updateVersionInDB(tx, currentVer); err != nil {
 			return err
 		}
-		currentVer++
-	}
 
-	// Downgrade to target version.
-	for currentVer > targetVer {
-		if err = migrations[currentVer-1].StepDown(tx); err != nil {
-			_ = tx.Rollback()
-			return err
-		}
-		currentVer--
-	}
-
-	if err = updateVersionInDB(tx, currentVer); err != nil {
-		_ = tx.Rollback()
-		return err
-	}
-
-	return tx.Commit()
+		return nil
+	})
 }
---
+++ migration/sql.go
@@ -0,0 +1,26 @@
+package migration
+
+import "database/sql"
+
+func withTransaction(db *sql.DB, txFunc func(*sql.Tx) error) (errOut error) {
+	tx, errOut := db.Begin()
+	if errOut != nil {
+		return
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			// panic again
+			panic(p)
+		} else if errOut != nil {
+			_ = tx.Rollback()
+		} else {
+			errOut = tx.Commit()
+		}
+	}()
+
+	errOut = txFunc(tx)
+
+	return
+}
