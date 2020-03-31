bf665de97313e9b9ae6ad1b7577e56d033204438
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-31 15:23:33 +0200

    Reproducer for issue #385

--- tests/rest/rule_vote.go
+++ tests/rest/rule_vote.go
@@ -15,3 +15,34 @@ limitations under the License.
 */
 
 package tests
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/verdverm/frisby"
+)
+
+func constructURLVoteForRule(clusterID string, ruleID int) string {
+	return fmt.Sprintf("%sclusters/%s/rules/%d/like", apiURL, clusterID, ruleID)
+}
+
+// reproducerForIssue385 checks whether the issue https://github.com/RedHatInsights/insights-results-aggregator/issues/385 has been fixed
+func reproducerForIssue385() {
+	url := constructURLVoteForRule("000000000000000000000000000000000000", 0)
+	f := frisby.Create("Reproducer for issue #385 (https://github.com/RedHatInsights/insights-results-aggregator/issues/385)").Put(url)
+	setAuthHeader(f)
+	f.Send()
+	f.ExpectStatus(400)
+	f.ExpectHeader(contentTypeHeader, ContentTypeJSON)
+
+	statusResponse := readStatusFromResponse(f)
+	if statusResponse.Status == "ok" {
+		f.AddError(fmt.Sprintf("Expected error status, but got '%s' instead", statusResponse.Status))
+	}
+	if !strings.Contains(statusResponse.Status, "Error: 'invalid UUID format'") {
+		f.AddError("Unexpected error reported")
+	}
+
+	f.PrintReport()
+}
