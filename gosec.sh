928f203f5cac169ea31b8e6024221b9aeee6de70
Serhii Zakharov <szakharo@redhat.com>
2020-03-31 14:46:18 +0200

    fixed gosec

--- gosec.sh
+++ gosec.sh
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export GO111MODULE=off
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
