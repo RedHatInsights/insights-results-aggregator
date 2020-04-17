970ba38767e1ef96adc5b1e582d22ce08806e705
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-17 09:17:23 +0200

    Updated script

--- utils/gen_broken_messages.py
+++ utils/gen_broken_messages.py
@@ -20,6 +20,11 @@ This script read input message (that should be coorect) and generates bunch of
 new messages. Each generated message is broken in some way so it is possible
 to use such messages to test how broken messages are handled on aggregator
 (ie. consumer) side.
+
+Types of input message mutation:
+* any item (identified by its key) can be removed
+* new items with random key and content can be added
+* any item can be replaced by new random content
 """
 
 import json
@@ -27,6 +32,13 @@ import os
 import sys
 import copy
 import itertools
+import random
+
+from random_payload_generator import RandomPayloadGenerator
+
+
+added_counter = 0
+mutated_counter = 0
 
 
 def load_json(filename):
@@ -49,6 +61,20 @@ def filename_removed_items(removed_keys, selector=None):
         return "broken_without_attribute_" + filename
     else:
         return "broken_without_" + selector + "_attribute_" + filename
+
+
+def filename_added_items():
+    """Generate filename for JSON with items added into original payload."""
+    global added_counter
+    added_counter += 1
+    return "broken_added_items_{:03d}.json".format(added_counter)
+
+
+def filename_mutated_items():
+    """Generate filename for JSON with items added into original payload."""
+    global mutated_counter
+    mutated_counter += 1
+    return "broken_mutated_items_{:03d}.json".format(mutated_counter)
 
 
 def remove_items_one_iter(original_payload, items_count, remove_flags,
@@ -94,11 +120,57 @@ def remove_items(original_payload, selector=None):
                               selector)
 
 
+def add_items_one_iter(original_payload, how_many):
+    """One iteration of algorithm to add items into original payload."""
+    # deep copy
+    new_payload = copy.deepcopy(original_payload)
+    rpg = RandomPayloadGenerator()
+
+    for i in range(how_many):
+        new_key = rpg.generate_random_key_for_dict(new_payload)
+        new_value = rpg.generate_random_payload()
+        new_payload[new_key] = new_value
+
+    filename = filename_added_items()
+    generate_output(filename, new_payload)
+
+
+def add_random_items(original_payload, min, max, mutations):
+    """Algorithm to add items with random values into original payload."""
+    for how_many in range(min, max):
+        for i in range(1, mutations):
+            add_items_one_iter(original_payload, how_many)
+
+
+def mutate_items_one_iteration(original_payload, how_many):
+    """One iteration of algorithm to mutate items in original payload."""
+    # deep copy
+    new_payload = copy.deepcopy(original_payload)
+    rpg = RandomPayloadGenerator()
+
+    for i in range(how_many):
+        selected_key = random.choice(list(original_payload.keys()))
+        new_value = rpg.generate_random_payload()
+        new_payload[selected_key] = new_value
+
+    filename = filename_mutated_items()
+    generate_output(filename, new_payload)
+
+
+def mutate_items(original_payload, min, max):
+    """Algorithm to mutate items with random values in original payload."""
+    for how_many in range(1, len(original_payload)):
+        for i in range(min, max):
+            mutate_items_one_iteration(original_payload, how_many)
+
+
 def main(filename):
     """Entry point to this script."""
     original_payload = load_json(filename)
     remove_items(original_payload)
     remove_items(original_payload, "Report")
+    add_random_items(original_payload, 1, 5, 4)
+    mutate_items(original_payload, 1, 5)
 
 
 if len(sys.argv) < 2:
