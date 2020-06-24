55389088b224471c3468355319090db548e92cce
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-24 07:32:35 +0200

    Don't install goconst if it's already installed and executable.

--- goconst.sh
+++ goconst.sh
@@ -14,7 +14,11 @@
 # limitations under the License.
 
 
-GO111MODULE=off go get github.com/jgautheron/goconst/cmd/goconst
+if ! [ -x "$(command -v goconst)" ]
+then
+    echo -e "${BLUE}Installing goconst${NC}"
+    GO111MODULE=off go get github.com/jgautheron/goconst/cmd/goconst
+fi
 
 if [[ $(goconst -min-occurrences=3 ./... | tee /dev/tty | wc -l) -ne 0 ]]
 then
