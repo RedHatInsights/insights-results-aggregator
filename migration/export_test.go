66bc6725ee5e2f3701c1ad3861b37dc1d38d0ff3
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-28 09:09:48 +0200

    Updated comments for export_test.go sources

--- migration/export_test.go
+++ migration/export_test.go
@@ -17,6 +17,14 @@ limitations under the License.
 package migration
 
 // Export for testing
+//
+// This source file contains name aliases of all package-private functions
+// that need to be called from unit tests. Aliases should start with uppercase
+// letter because unit tests belong to different package.
+//
+// Please look into the following blogpost:
+// https://medium.com/@robiplus/golang-trick-export-for-test-aa16cbd7b8cd
+// to see why this trick is needed.
 var (
 	Migrations                           = &migrations
 	WithTransaction                      = withTransaction
--- storage/export_test.go
+++ storage/export_test.go
@@ -22,6 +22,10 @@ import (
 
 // Export for testing
 //
+// This source file contains name aliases of all package-private functions
+// that need to be called from unit tests. Aliases should start with uppercase
+// letter because unit tests belong to different package.
+//
 // Please look into the following blogpost:
 // https://medium.com/@robiplus/golang-trick-export-for-test-aa16cbd7b8cd
 // to see why this trick is needed.
