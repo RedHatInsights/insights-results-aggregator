fab657f36b4c5f71d06b46f6e41f7e62819afca4
Serhii Zakharov <szakharo@redhat.com>
2020-08-27 19:09:08 +0200

    enabled caching

--- server/endpoints.go
+++ server/endpoints.go
@@ -93,6 +93,6 @@ func (server *HTTPServer) addEndpointsToRouter(router *mux.Router) {
 	// OpenAPI specs
 	router.HandleFunc(
 		openAPIURL,
-		httputils.CreateOpenAPIHandler(server.Config.APISpecFile, server.Config.Debug),
+		httputils.CreateOpenAPIHandler(server.Config.APISpecFile, server.Config.Debug, true),
 	).Methods(http.MethodGet)
 }
