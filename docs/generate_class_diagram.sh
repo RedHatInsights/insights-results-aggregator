46565b64b57985b021a57a24bf3a01f759d3b713
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-09 09:41:46 +0200

    Updated script to create class diagram

--- docs/generate_class_diagram.sh
+++ docs/generate_class_diagram.sh
@@ -13,10 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# In order to generate class diagram, `plantuml.jar` needs to be installed
+# and added into `CLASSPATH`. The Java archive file `plantuml.jar` is available
+# at https://plantuml.com/download. It is possible just to download the Java
+# archive into this directory and not to update `CLASSPATH` environment
+# variable.
+#
+# More info about `CLASSPATH` is available at
+# https://docs.oracle.com/javase/tutorial/essential/environment/paths.html
+
 pushd ..
 goplantuml -recursive . > class_diagram.uml
 java -jar ~/tools/plantuml.jar class_diagram.uml
 java -jar ~/tools/plantuml.jar -tsvg class_diagram.uml
+xmllint --format class_diagram.svg > class_diagram_.svg
+mv class_diagram_.svg class_diagram.svg
 mv class_diagram.uml docs/
 mv class_diagram.svg docs/
 mv class_diagram.png docs/
