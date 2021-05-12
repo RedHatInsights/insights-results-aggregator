f17658ccf786d12ad45d4610cf6631979a5de38d
Dmitry Misharov <dmisharo@redhat.com>
2021-05-12 11:43:14 +0200

    Temporary stub for AppSRE jenkins

--- pr_check.sh
+++ pr_check.sh
@@ -12,6 +12,10 @@ IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
 
 
+# Temporary stub
+mkdir artifacts
+touch artifacts/junit-stub.xml 
+
 # Install bonfire repo/initialize
 # CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 # curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
