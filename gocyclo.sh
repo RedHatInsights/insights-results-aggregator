d055a6c47d5e56955caefe2eb733440ea7120029
José Luis Segura Lucas <joseluis@redhat.com>
2021-07-20 12:14:50 +0200

    Update tooling to run until go-toolset 1.16 is released

--- gocyclo.sh
+++ gocyclo.sh
@@ -17,7 +17,7 @@
 if ! [ -x "$(command -v gocyclo)" ]
 then
     echo -e "${BLUE}Installing gocyclo${NC}"
-    GO111MODULE=off go get github.com/fzipp/gocyclo/cmd/gocyclo
+    go get github.com/fzipp/gocyclo/cmd/gocyclo
 fi
 
 gocyclo -over 9 -avg .
