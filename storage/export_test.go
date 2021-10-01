93ececa0453d2a48fc0a573622c68c630585f678
Papa Bakary Camara <pcamara@redhat.com>
2021-10-01 14:59:02 +0200

    Use named returns in export_test::InsertRecommendations function

--- storage/export_test.go
+++ storage/export_test.go
@@ -56,12 +56,12 @@ func SetClustersLastChecked(storage *DBStorage, cluster types.ClusterName, lastC
 	storage.clustersLastChecked[cluster] = lastChecked
 }
 
-func InsertRecommendations(storage *DBStorage, orgID types.OrgID, clusterName types.ClusterName, report types.ReportRules) (int, error) {
+func InsertRecommendations(storage *DBStorage, orgID types.OrgID, clusterName types.ClusterName, report types.ReportRules) (inserted int, err error) {
 	tx, err := storage.connection.Begin()
 	if err != nil {
 		return 0, err
 	}
-	inserted, err := storage.insertRecommendations(tx, orgID, clusterName, report)
+	inserted, err = storage.insertRecommendations(tx, orgID, clusterName, report)
 	if err != nil {
 		_ = tx.Rollback()
 		return 0, err
