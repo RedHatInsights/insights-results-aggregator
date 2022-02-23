e8d5cba30ea38a3f2bd00147dfe60ce2b160160f
Daniel Bily <dbily@redhat.com>
2022-02-23 14:22:24 +0100

    disable migration 0022 to check problem

--- migration/migrations.go
+++ migration/migrations.go
@@ -38,5 +38,5 @@ var migrations = []Migration{
 	mig0019ModifyRecommendationTable,
 	mig0020ModifyAdvisorRatingsTable,
 	mig0021AddGatheredAtToReport,
-	mig0022CleanupEnableDisableTables,
+	//mig0022CleanupEnableDisableTables,
 }
