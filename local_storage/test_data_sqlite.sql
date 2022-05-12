b1f583ee2e0a922c2546ab6979b5c6d94c188ab0
Juan Díaz <juandiazsuarez0@gmail.com>
2022-05-12 09:32:30 +0200

    add REST API tests to clusters_detail endpoint (#1612)

--- local_storage/test_data_sqlite.sql
+++ local_storage/test_data_sqlite.sql
@@ -24,4 +24,15 @@ insert into report (org_id, cluster, report, reported_at, last_checked_at) value
 insert into report (org_id, cluster, report, reported_at, last_checked_at) values (4, 'addddddd-bbbb-0000-0000-000000000000', '{}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 insert into report (org_id, cluster, report, reported_at, last_checked_at) values (4, 'addddddd-bbbb-cccc-0000-000000000000', '{}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+delete from recommendation;
+
+INSERT INTO recommendation (org_id, cluster_id, rule_fqdn, error_key, rule_id, created_at) VALUES (1, '11111111-1111-1111-1111-111111111111', 'ccx_rules_ocp.external.rules.node_installer_degraded', 'ek1', 'ccx_rules_ocp.external.rules.node_installer_degraded|ek1', CURRENT_TIMESTAMP);
+INSERT INTO recommendation (org_id, cluster_id, rule_fqdn, error_key, rule_id, created_at) VALUES (2, '22222222-2222-2222-2222-222222222222', 'ccx_rules_ocp.external.rules.node_installer_degraded', 'ek1', 'ccx_rules_ocp.external.rules.node_installer_degraded|ek1', CURRENT_TIMESTAMP);
+INSERT INTO recommendation (org_id, cluster_id, rule_fqdn, error_key, rule_id, created_at) VALUES (3, '33333333-3333-3333-3333-333333333333', 'ccx_rules_ocp.external.rules.node_installer_degraded', 'ek1', 'ccx_rules_ocp.external.rules.node_installer_degraded|ek1', CURRENT_TIMESTAMP);
+
+delete from report_info;
+
+INSERT INTO report_info(org_id, cluster_id, version_info) VALUES (1, '11111111-1111-1111-1111-111111111111', '1.0');
+INSERT INTO report_info(org_id, cluster_id, version_info) VALUES (2, '22222222-2222-2222-2222-222222222222', '');
+
 delete from cluster_rule_user_feedback;
