4aee343309bf78ab0ef9b7882b9ffc28e7b0b430
Daniel Bily <dbily@redhat.com>
2022-08-14 19:47:12 +0200

    update auth mechanism to accomodate org_id/account_number migration; partially remove unused 'jwt' auth type

--- server/auth_test.go
+++ server/auth_test.go
@@ -34,15 +34,6 @@ var configAuth = server.Configuration{
 	MaximumFeedbackMessageLength: 255,
 }
 
-var configAuth2 = server.Configuration{
-	Address:                      ":8080",
-	APIPrefix:                    "/api/test/",
-	Debug:                        true,
-	Auth:                         true,
-	AuthType:                     "jwt",
-	MaximumFeedbackMessageLength: 255,
-}
-
 // TestMissingAuthToken checks how the missing auth. token header (expected in HTTP request) is handled
 func TestMissingAuthToken(t *testing.T) {
 	helpers.AssertAPIRequest(t, nil, &configAuth, &helpers.APIRequest{
@@ -52,47 +43,6 @@ func TestMissingAuthToken(t *testing.T) {
 	}, &helpers.APIResponse{
 		StatusCode: http.StatusUnauthorized,
 		Body:       `{"status": "Missing auth token"}`,
-	})
-}
-
-// TestJWTToken checks authorization through Authorization header
-func TestJWTToken(t *testing.T) {
-	helpers.AssertAPIRequest(t, nil, &configAuth2, &helpers.APIRequest{
-		Method:             http.MethodGet,
-		Endpoint:           server.ClustersForOrganizationEndpoint,
-		EndpointArgs:       []interface{}{1234},
-		AuthorizationToken: "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X251bWJlciI6IjUyMTM0NzYiLCJvcmdfaWQiOiIxMjM0In0.Y9nNaZXbMEO6nz2EHNaCvHxPM0IaeT7GGR-T8u8h_nr_2b5dYsCQiZGzzkBupRJruHy9K6acgJ08JN2Q28eOAEVk_ZD2EqO43rSOS6oe8uZmVo-nCecdqovHa9PqW8RcZMMxVfGXednw82kKI8j1aT_nbJ1j9JZt3hnHM4wtqydelMij7zKyZLHTWFeZbDDCuEIkeWA6AdIBCMdywdFTSTsccVcxT2rgv4mKpxY1Fn6Vu_Xo27noZW88QhPTHbzM38l9lknGrvJVggrzMTABqWEXNVHbph0lXjPWsP7pe6v5DalYEBN2r3a16A6s3jPfI86cRC6_oeXotlW6je0iKQ",
-	}, &helpers.APIResponse{
-		StatusCode: http.StatusOK,
-		Body:       `{"clusters":[],"status":"ok"}`,
-	})
-}
-
-// TestJWTToken checks authorization through Authorization header
-func TestJWTTokenMalformed(t *testing.T) {
-	helpers.AssertAPIRequest(t, nil, &configAuth2, &helpers.APIRequest{
-		Method:       http.MethodGet,
-		Endpoint:     server.ClustersForOrganizationEndpoint,
-		EndpointArgs: []interface{}{1234},
-		// do not pass token itself
-		AuthorizationToken: "Bearer",
-	}, &helpers.APIResponse{
-		StatusCode: http.StatusUnauthorized,
-		Body:       `{"status":"Invalid/Malformed auth token"}`,
-	})
-}
-
-// TestJWTTokenMalformedJSON checks authorization through Authorization header with bad JSON in JWT token
-func TestJWTTokenMalformedJSON(t *testing.T) {
-	helpers.AssertAPIRequest(t, nil, &configAuth2, &helpers.APIRequest{
-		Method:       http.MethodGet,
-		Endpoint:     server.ClustersForOrganizationEndpoint,
-		EndpointArgs: []interface{}{1234},
-		// pass bad json
-		AuthorizationToken: "Bearer bm90LWpzb24K.bm90LWpzb24K.bm90LWpzb24K",
-	}, &helpers.APIResponse{
-		StatusCode: http.StatusUnauthorized,
-		Body:       `{"status":"Malformed authentication token"}`,
 	})
 }
 
@@ -139,13 +89,13 @@ func TestInvalidJsonAuthToken(t *testing.T) {
 // TestBadOrganizationID checks if organization ID is checked properly
 func TestBadOrganizationID(t *testing.T) {
 	providedOrgID := 12345
-	orgIDInXRH := 1234
+	orgIDInXRH := 1
 	body := fmt.Sprintf(`{"status":"you have no permissions to get or change info about the organization with ID %v; you can access info about organization with ID %v"}`, providedOrgID, orgIDInXRH)
 	helpers.AssertAPIRequest(t, nil, &configAuth, &helpers.APIRequest{
 		Method:       http.MethodGet,
 		Endpoint:     server.ClustersForOrganizationEndpoint,
 		EndpointArgs: []interface{}{providedOrgID},
-		XRHIdentity:  "eyJpZGVudGl0eSI6IHsiaW50ZXJuYWwiOiB7Im9yZ19pZCI6ICIxMjM0In19fQo=",
+		XRHIdentity:  "eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMDAwMDAwMSIsICJpbnRlcm5hbCI6IHsib3JnX2lkIjogIjEifX19Cg==",
 	}, &helpers.APIResponse{
 		StatusCode: http.StatusForbidden,
 		Body:       body,
