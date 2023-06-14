73e6625358559cf22d9c4be7781faea3d3e4fddf
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-14 09:15:40 +0200

    Support for ClientID

--- broker/configuration.go
+++ broker/configuration.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 Red Hat, Inc.
+Copyright © 2020, 2023 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -45,6 +45,7 @@ type Configuration struct {
 	Enabled              bool          `mapstructure:"enabled" toml:"enabled"`
 	OrgAllowlist         mapset.Set    `mapstructure:"org_allowlist_file" toml:"org_allowlist_file"`
 	OrgAllowlistEnabled  bool          `mapstructure:"enable_org_allowlist" toml:"enable_org_allowlist"`
+	ClientID             string        `mapstructure:"client_id" toml:"client_id"`
 }
 
 // SaramaConfigFromBrokerConfig returns a Config struct from broker.Configuration parameters
@@ -76,5 +77,13 @@ func SaramaConfigFromBrokerConfig(cfg Configuration) (*sarama.Config, error) {
 		saramaConfig.Net.SASL.Password = cfg.SaslPassword
 		saramaConfig.Net.SASL.Mechanism = sarama.SASLMechanism(cfg.SaslMechanism)
 	}
+
+	// ClientID is fully optional, but by setting it, we can get rid of some warning messages in logs
+	if cfg.ClientID != "" {
+		// if not set, the "sarama" will be used instead
+		saramaConfig.ClientID = cfg.ClientID
+	}
+
+	// now the config structure is filled-in
 	return saramaConfig, nil
 }
