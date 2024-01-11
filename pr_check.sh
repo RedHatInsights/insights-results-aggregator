47b0fa18f446b263ac334a3a0be2ad3a7e81af45
Martin Zibricky <matysek@users.noreply.github.com>
2024-01-11 15:05:02 +0100

    Update pr_check.sh use REF_ENV ephemeral.

--- pr_check.sh
+++ pr_check.sh
@@ -20,7 +20,9 @@ set -exv
 # Options that must be configured by app owner
 # --------------------------------------------
 APP_NAME="ccx-data-pipeline"  # name of app-sre "application" folder this component lives in
-REF_ENV="insights-production"
+# TODO set it to "insights-stage" once we have dvo-writer/dvo-extractor stage deploy config
+# TODO set it back to "insights-production" once we have dvo deployed to prod
+REF_ENV="ephemeral"
 # NOTE: insights-results-aggregator contains deployment for multiple services
 #       for pull requests we need latest git PR version of these components to be
 #       deployed to ephemeral env and overriding resource template --set-template-ref.
