cf42bd4e2e8cecaabf03a09a64ebb2dcf47bef57
Juan Díaz <jdiazsua@redhat.com>
2024-03-12 13:04:05 +0100

    [CCXDEV-12602] Add migration to delete buggy rows (#1987)
    * add migration to delete buggy rows
    
    * fix typo in table name
    
    * fix lint
    
    * fix typo in date

---
+++ migration/dvomigrations/mig_0003_ccxdev_12602_delete_buggy_records.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2020 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dvomigrations
+
+import (
+	"database/sql"
+
+	"github.com/RedHatInsights/insights-results-aggregator/migration"
+	"github.com/RedHatInsights/insights-results-aggregator/types"
+)
+
+var mig0003CCXDEV12602DeleteBuggyRecords = migration.Migration{
+	StepUp: func(tx *sql.Tx, _ types.DBDriver) error {
+		_, err := tx.Exec(`
+			DELETE FROM dvo.dvo_report WHERE last_checked_at <= DATE '2024-03-08';
+		`)
+		return err
+	},
+	StepDown: func(_ *sql.Tx, _ types.DBDriver) error { return nil },
+}
