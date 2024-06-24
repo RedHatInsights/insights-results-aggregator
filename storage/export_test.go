c084f6863e8b6f7b260ffc159a0b1a543449e379
Papa Bakary Camara <pcamara@redhat.com>
2024-06-24 09:48:00 +0200

    Add UT to test CCXDEV-10244

--- storage/export_test.go
+++ storage/export_test.go
@@ -85,3 +85,7 @@ func InsertRecommendations(
 	_ = tx.Commit()
 	return inserted, nil
 }
+
+func GetRuleKeyCreatedAtMap(storage *OCPRecommendationsDBStorage, query string, orgID types.OrgID, clusterName types.ClusterName) (map[string]types.Timestamp, error) {
+	return storage.getRuleKeyCreatedAtMap(query, orgID, clusterName)
+}
