29dadd2565e40973cb79ba506b335c1271a693e6
Iker Reyes <ireyes@redhat.com>
2024-11-04 11:55:15 +0100

    New table for runtimes heartbeats
    A migration included in the DVO ones (for simplicity) creates a new table
    to store runtimes heartbeats data (basically ID and timestamp).

---
+++ migration/dvomigrations/mig_0005_runtimes_heartbeats_last_check.go
@@ -0,0 +1,36 @@
+
+package dvomigrations
+
+import (
+	"database/sql"
+
+	"github.com/RedHatInsights/insights-results-aggregator/migration"
+	"github.com/RedHatInsights/insights-results-aggregator/types"
+)
+
+var mig0005CreateRuntimesHeartbeats = migration.Migration{
+	StepUp: func(tx *sql.Tx, _ types.DBDriver) error {
+		_, err := tx.Exec(`
+			CREATE TABLE runtimes.heartbeats (
+				instance_id     VARCHAR NOT NULL
+				last_checked_at TIMESTAMP,
+				PRIMARY KEY(instance_id)
+			);
+		`)
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.Exec(`
+			COMMENT ON TABLE runtimes.heartbeats IS 'This table is used to store information of when the hearbeats was last received.';
+			COMMENT ON COLUMN runtimes.heartbeats.instance_id IS 'instance ID';
+			COMMENT ON COLUMN runtimes.heartbeats.last_checked_at IS 'timestamp of the received heartbeat';
+		`)
+
+		return err
+	},
+	StepDown: func(tx *sql.Tx, _ types.DBDriver) error {
+		_, err := tx.Exec(`DROP TABLE runtimes.heartbeats;`)
+		return err
+	},
+}
