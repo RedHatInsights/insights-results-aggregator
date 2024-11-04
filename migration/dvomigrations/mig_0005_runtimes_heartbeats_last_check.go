60d00ecddc44023a11d1e5ce34ae6bae9ad70d7d
Iker Reyes <ireyes@redhat.com>
2024-11-04 13:02:59 +0100

    Rename runtimes.heartbeats to dvo.runtimes_heartbeats to avoid issues with DBschema

--- migration/dvomigrations/mig_0005_runtimes_heartbeats_last_check.go
+++ migration/dvomigrations/mig_0005_runtimes_heartbeats_last_check.go
@@ -10,7 +10,7 @@ import (
 var mig0005CreateRuntimesHeartbeats = migration.Migration{
 	StepUp: func(tx *sql.Tx, _ types.DBDriver) error {
 		_, err := tx.Exec(`
-			CREATE TABLE runtimes.heartbeats (
+			CREATE TABLE dvo.runtimes_heartbeats (
 				instance_id     VARCHAR NOT NULL,
 				last_checked_at TIMESTAMP,
 				PRIMARY KEY(instance_id)
@@ -21,15 +21,15 @@ var mig0005CreateRuntimesHeartbeats = migration.Migration{
 		}
 
 		_, err = tx.Exec(`
-			COMMENT ON TABLE runtimes.heartbeats IS 'This table is used to store information of when the hearbeats was last received.';
-			COMMENT ON COLUMN runtimes.heartbeats.instance_id IS 'instance ID';
-			COMMENT ON COLUMN runtimes.heartbeats.last_checked_at IS 'timestamp of the received heartbeat';
+			COMMENT ON TABLE dvo.runtimes_heartbeats IS 'This table is used to store information of when the hearbeats was last received.';
+			COMMENT ON COLUMN dvo.runtimes_heartbeats.instance_id IS 'instance ID';
+			COMMENT ON COLUMN dvo.runtimes_heartbeats.last_checked_at IS 'timestamp of the received heartbeat';
 		`)
 
 		return err
 	},
 	StepDown: func(tx *sql.Tx, _ types.DBDriver) error {
-		_, err := tx.Exec(`DROP TABLE runtimes.heartbeats;`)
+		_, err := tx.Exec(`DROP TABLE dvo.runtimes_heartbeats;`)
 		return err
 	},
 }
